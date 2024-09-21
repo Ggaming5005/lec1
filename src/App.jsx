a7a2fcc8a04ca5335635a3c8ab8e477e74acfe20
Ggaming5005 <147240147+sabagogberashvili@users.noreply.github.com>
2024-09-21 05:43:15 -0700

    commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  const firstname = "hello this is  my first project";
+
+  return (
+    <div>
+      <h1 className="title">giorgi {firstname}</h1>
+    </div>
+  );
+}
+
+export default App;
